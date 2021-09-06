edd2de07c725cfe9c0815f3fd45c2daedc975adb
dkurganskii <44755635+dkurganskii@users.noreply.github.com>
2021-09-06 09:25:57 -0500

    create imageCard component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Search from './components/Search';
+import ImageCard from './components/ImageCard';
 
 const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY
 
@@ -17,7 +18,7 @@ function App() {
     fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`)
       .then((res) => res.json())
       .then((data) => {
-        setImages([data, ...images])
+        setImages([{ ...data, title: word }, ...images])
         console.log(images)
       })
       .catch((err) => {
@@ -30,6 +31,7 @@ function App() {
     <div >
       <Header title="Images Gallery" />
       <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
+      {!!images.length && <ImageCard image={images[0]} />}
     </div>
   );
 }
