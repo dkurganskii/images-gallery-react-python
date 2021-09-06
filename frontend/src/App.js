d42f701b913dd9e851e0c7ebe73e34eccd9e1687
dkurganskii <44755635+dkurganskii@users.noreply.github.com>
2021-09-06 08:32:58 -0500

    save images in the state

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,7 @@ const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY
 function App() {
 
   const [word, setWord] = useState('')
+  const [images, setImages] = useState('')
 
   const handleSearchSubmit = (e) => {
     e.preventDefault();
@@ -16,7 +17,8 @@ function App() {
     fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`)
       .then((res) => res.json())
       .then((data) => {
-        console.log(data)
+        setImages([data, ...images])
+        console.log(images)
       })
       .catch((err) => {
         console.log(err)
