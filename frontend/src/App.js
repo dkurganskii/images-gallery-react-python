a0cde4e135327cc928e75692b8ef6793a004fa8a
dkurganskii <44755635+dkurganskii@users.noreply.github.com>
2021-09-09 16:44:35 -0500

    display all images

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Search from './components/Search';
 import ImageCard from './components/ImageCard';
+import { Container, Row, Col } from 'react-bootstrap';
 
 const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY
 
@@ -10,7 +11,7 @@ const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY
 function App() {
 
   const [word, setWord] = useState('')
-  const [images, setImages] = useState('')
+  const [images, setImages] = useState([])
 
   const handleSearchSubmit = (e) => {
     e.preventDefault();
@@ -27,11 +28,23 @@ function App() {
     setWord('')
   }
 
+  const handleDeleteImage = (id) => {
+    setImages(images.filter((image) => image.id !== id));
+  };
+
   return (
     <div >
       <Header title="Images Gallery" />
       <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
-      {!!images.length && <ImageCard image={images[0]} />}
+      <Container className="mt-4">
+        <Row xs={1} md={2} lg={3}>
+          {images.map((image, i) => (
+            <Col key={i} className="pb-3">
+              <ImageCard image={image} deleteImage={handleDeleteImage} />
+            </Col>
+          ))}
+        </Row>
+      </Container>
     </div>
   );
 }
