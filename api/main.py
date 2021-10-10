5d510ea2b5bf35f7fbb6f3704ffb0087e6f99b08
dkurganskii <44755635+dkurganskii@users.noreply.github.com>
2021-10-10 08:35:33 -0500

    create API request to Unsplash API

--- api/main.py
+++ api/main.py
@@ -1,4 +1,9 @@
+import requests
 from flask import Flask, request
+from requests.api import head
+
+UNSPLASH_URL = 'https://api.unsplash.com/photos/random'
+UNSPLASH_KEY = ''
 
 app = Flask(__name__)
 
@@ -6,7 +11,16 @@ app = Flask(__name__)
 @app.route("/new-image")
 def new_image():
     word = request.args.get("query")
-    return {"word": word}
+    headers = {
+        'Accept-Version': 'v1',
+        'Authorization': 'Client-ID ' + UNSPLASH_KEY
+    }
+    params = {
+        "query": word
+    }
+    response = requests.get(url=UNSPLASH_URL, headers=headers, params=params)
+    data = response.json()
+    return data
 
 
 if __name__ == "__main__":
