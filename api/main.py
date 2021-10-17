33281d6ada57e96582db8c4ff1ccbd353be5c8da
dkurganskii <44755635+dkurganskii@users.noreply.github.com>
2021-10-17 15:11:22 -0500

    implement env variables

--- api/main.py
+++ api/main.py
@@ -1,9 +1,17 @@
+import os
 import requests
 from flask import Flask, request
-from requests.api import head
+from dotenv import load_dotenv
+
+load_dotenv(dotenv_path="./.env.local")
+
 
 UNSPLASH_URL = 'https://api.unsplash.com/photos/random'
-UNSPLASH_KEY = ''
+UNSPLASH_KEY = os.environ.get("UNSPLASH_KEY", "")
+
+if not UNSPLASH_KEY:
+    raise EnvironmentError(
+        "Please create .env.local file and insert there UNSPLASH_KEY")
 
 app = Flask(__name__)
 
